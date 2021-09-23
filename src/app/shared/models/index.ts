27a452a33bb6ff332c867c87ecfefbb9a147376d
Rodrigo Balaban Zbytkowski <rodrigo.balaban@gmail.com>
2021-09-22 22:09:34 -0300

    Update barrel file SharedModule

--- src/app/shared/models/index.ts
+++ src/app/shared/models/index.ts
@@ -2,3 +2,5 @@ export * from './establishment.model';
 export * from './establishment-category.model';
 export * from './location.model';
 export * from './office-hour.model';
+export * from './user.model';
+export * from './user-sus.model';
