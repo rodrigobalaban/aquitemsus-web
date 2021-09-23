5ca00f6fed4ab002defd2386c808d522da7b5d3d
Rodrigo Balaban Zbytkowski <rodrigo.balaban@gmail.com>
2021-09-22 22:00:00 -0300

    Create User's model classes

---
+++ src/app/shared/models/user-sus.model.spec.ts
@@ -0,0 +1,7 @@
+import { UserSUS } from './user-sus.model';
+
+describe('UserSUS', () => {
+  it('should create an instance', () => {
+    expect(new UserSUS()).toBeTruthy();
+  });
+});
---
+++ src/app/shared/models/user-sus.model.ts
@@ -0,0 +1,6 @@
+import { Location } from ".";
+import { User } from "./user.model";
+
+export class UserSUS extends User {
+    public location?: Location;
+}
