b5fdc429da144a44a76c27022b88b16243d57087
Rodrigo Balaban Zbytkowski <rodrigo.balaban@gmail.com>
2021-10-01 00:32:46 -0300

    Create GoogleMaps Service

---
+++ src/app/modules/establishments-map/services/google-maps.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GoogleMapsService {
+  constructor(private httpClient: HttpClient) {}
+
+  loadGoogleMapApi(): Promise<boolean> {
+    return this.httpClient
+      .jsonp(
+        'https://maps.googleapis.com/maps/api/js?key=' +
+          environment.googleMapsApiKey,
+        'callback'
+      )
+      .pipe(
+        map(() => true),
+        catchError(() => of(false))
+      )
+      .toPromise();
+  }
+}
