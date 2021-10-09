3ca96a39c00fd3ae26f4789aeff91d8a2922403b
Rodrigo Balaban Zbytkowski <rodrigo.balaban@gmail.com>
2021-10-09 15:52:38 -0300

    Changes static to real data in establishment modal

--- src/app/modules/establishments-map/modal/modal.component.ts
+++ src/app/modules/establishments-map/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Establishment } from 'src/app/shared';
 
@@ -11,12 +11,38 @@ export interface DialogData {
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss'],
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent {
   establishment: Establishment;
 
   constructor(@Inject(MAT_DIALOG_DATA) private data: DialogData) {
     this.establishment = data.establishment;
   }
 
-  ngOnInit(): void {}
+  getWeekdayDescription(dayOfWeek: number): string {
+    switch (dayOfWeek) {
+      case 0: {
+        return 'Domingo';
+      }
+      case 1: {
+        return 'Segunda';
+      }
+      case 2: {
+        return 'Terça';
+      }
+      case 3: {
+        return 'Quarta';
+      }
+      case 4: {
+        return 'Quinta';
+      }
+      case 5: {
+        return 'Sexta';
+      }
+      case 6: {
+        return 'Sábado';
+      }
+    }
+
+    return '';
+  }
 }
